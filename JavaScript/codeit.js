76b5996ecdd2461bcf4cb7f44fb46c5c14679a95
Yujin  Jung <wjd1dbwls@gmail.com>
2022-01-13 23:31:54 +0900

    JavaScript : basic summary (1)

--- JavaScript/codeit.js
+++ JavaScript/codeit.js
@@ -1,1 +1,7 @@
-console.log('Hello Codeit!')
+// 변수 선언
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let mochaPrice = 4800;
+
+console.log(espressoPrice*2);
+comsole.log(lattePrice*4 + mochaPrice*2);
