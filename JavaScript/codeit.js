9da9cc3de180d2ccd6ebf5ed1108cca5f8418160
Yujin  Jung <wjd1dbwls@gmail.com>
2022-01-14 21:53:46 +0900

    JavaScript : basic summary(2)

--- JavaScript/codeit.js
+++ JavaScript/codeit.js
@@ -5,3 +5,22 @@ let mochaPrice = 4800;
 
 console.log(espressoPrice*2);
 comsole.log(lattePrice*4 + mochaPrice*2);
+
+
+function greeting() {
+  console.log('Hi');
+  console.log('안녕');
+  console.log('Bonjour');
+};
+
+function welcome(name) {
+  console.log('안녕하세요 ' + name + '님!');
+};
+
+function printSquare(x) {
+  console.log(x * x);
+};
+
+greeting();
+welcome('코드잇');
+printSquare(2);
