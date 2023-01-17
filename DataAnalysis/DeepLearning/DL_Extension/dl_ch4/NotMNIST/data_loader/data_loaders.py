1125255dffedc625089bb65441b95ab0ef072a2f
backend_ai_juj <wjd1dbwls@gmail.com>
2023-01-17 12:25:01 +0000

    DL Extension : dataloader meeting - why PILImage

--- DataAnalysis/DeepLearning/DL_Extension/dl_ch4/NotMNIST/data_loader/data_loaders.py
+++ DataAnalysis/DeepLearning/DL_Extension/dl_ch4/NotMNIST/data_loader/data_loaders.py
@@ -49,9 +49,12 @@ class NotMnistDataset(Dataset):
     
     def __getitem__(self, idx):
         img_path, target = self.data[idx], self.targets[idx]
+        # read_image : 이미지 파일을 Tensor로 바꾸나 dtype이 uint8로 설정됨 (transform의 다른 함수를 적용할 수 없음)
         img = read_image(str(img_path))
-        img = transforms.ToPILImage()(img.cpu()) # ToPILImage ? cpu ?
+        # ToPILImage : Tensor type을 PILImage 형태로  변경
+        img = transforms.ToPILImage()(img.cpu())
         if self.transform is not None:
+            # transform 안의 ToTensor가 PILImage를 Tensor로 다시 변환
             img = self.transform(img)    
         target = self.classes.index(target)
         target = torch.tensor(target)
