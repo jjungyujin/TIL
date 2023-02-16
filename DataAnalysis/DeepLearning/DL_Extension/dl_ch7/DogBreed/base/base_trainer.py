6354dee8a62d170d15a29d9a6d6c38eb7c9bbc05
JungYujin <wjd1dbwls@gmail.com>
2023-02-16 22:02:55 +0900

    Dog Breeds : save every checkpoint

--- DataAnalysis/DeepLearning/DL_Extension/dl_ch7/DogBreed/base/base_trainer.py
+++ DataAnalysis/DeepLearning/DL_Extension/dl_ch7/DogBreed/base/base_trainer.py
@@ -103,9 +103,10 @@ class BaseTrainer:
                     self.logger.info("Validation performance didn\'t improve for {} epochs. "
                                      "Training stops.".format(self.early_stop))
                     break
-
-            if epoch % self.save_period == 0:
-                self._save_checkpoint(epoch, save_best=best)
+                
+            self._save_checkpoint(epoch, save_best=best)
+            
+            if epoch % self.save_period == 0:    
                 dingdong_bell.post_dingdong(epoch, log, stack_df)
 
     
