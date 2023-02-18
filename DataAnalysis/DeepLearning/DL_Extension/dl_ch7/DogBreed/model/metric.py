dc88f17b5a3eb274b3d7daf058d77936bf9df785
JungYujin <wjd1dbwls@gmail.com>
2023-02-19 00:54:23 +0900

    Dog Breeds : fix accuracy code for cutmix

--- DataAnalysis/DeepLearning/DL_Extension/dl_ch7/DogBreed/model/metric.py
+++ DataAnalysis/DeepLearning/DL_Extension/dl_ch7/DogBreed/model/metric.py
@@ -1,14 +1,21 @@
 import torch
 
-
 def accuracy(output, target):
     with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
-
+        if isinstance(target, (tuple, list)):
+            num = len(target)
+            _, preds = torch.max(output, dim=1)
+            targets1, targets2, lam = target
+            correct1 = preds.eq(targets1).sum().item()
+            correct2 = preds.eq(targets2).sum().item()
+            accuracy = (lam * correct1 + (1 - lam) * correct2) / num
+        else :
+            pred = torch.argmax(output, dim=1)
+            assert pred.shape[0] == len(target)
+            correct = 0
+            correct += torch.sum(pred == target).item()
+            accuracy = correct / len(target)
+    return accuracy
 
 def top_k_acc(output, target, k=3):
     with torch.no_grad():
